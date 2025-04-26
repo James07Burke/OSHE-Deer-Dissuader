db00845fba17132666eebb452e1f428861e73b4e
Riley Patterson <50743400+redrileyp@users.noreply.github.com>
2025-04-25 22:19:14 -0400

    Updated vision.py and removed unnecessary files

--- vision.py
+++ vision.py
@@ -1,41 +1,25 @@
 import cv2
-import time
 from ultralytics import YOLO
+import pygame
+from datetime import datetime
 
-# Loading custom yolo model
-yolo = YOLO('/home/redrileyp/DeerDissuader/runs/detect/yolov8_deerdetector/weights/best.pt')
+# Load model and initialize camera
+model = YOLO('/path/to/model/best.pt')
+cap = cv2.VideoCapture(0)
 
-# Opening video feed
-videoCap = cv2.VideoCapture('/home/redrileyp/DeerDissuader/deer.mp4')
-
-# Function to get class colors
-def getColors(cls_num):
-    base_colors = [(255,0,0), (0,255,0), (0,0,255)]
-    color_index = cls_num % len(base_colors)
-    increments = [(1,-2,1),(-2,1,-1), (1,-1,2)]
-    color = [base_colors[color_index][i] + increments[color_index][i] * (cls_num // len(base_colors)) % 256 for i in range(3)]
-    return tuple(color)
-
-frame_width = int(videoCap.get(cv2.CAP_PROP_FRAME_WIDTH))
-frame_height = int(videoCap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-fourcc = cv2.VideoWriter_fourcc('m','p','4','v')
-out = cv2.VideoWriter('output.avi', fourcc, 20.0, (frame_width, frame_height))
+pygame.mixer.init() # Initialize audio
 
 while True:
-    ret, frame = videoCap.read()
-    if not ret:
-        continue
-
-    # Processing frame to RGB for yolo
-    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    results = yolo.predict(rgb_frame, conf = 0.4) # Confidence prediction is 70%
+    ret, frame = cap.read() # Load frame
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # Formatting frame for easy model detection
+    results = model.predict(source=rgb_frame, verbose=False, stream=True, show=True)
 
     for result in results:
-        classes_names = result.names
+        class_name = result.names
         for box in result.boxes:
-            deerSeen = False
-            # Check if confidence is greater than 75%
-            if box.conf[0] > 0.5:
+            if len(result.boxes.cls) > 0.6 and box.conf[0] > 0.7:
+                dclass = result.boxes.cls[0].item()
+
                 # Get coords
                 [x1,y1,x2,y2] = box.xyxy[0]
                 # Convert to int
@@ -43,26 +27,20 @@ while True:
                 # Get class
                 cls = int(box.cls[0])
                 # Get class name
-                class_name = classes_names[cls]
-                # Get class color
-                color = getColors(cls)
+                class_name = class_name[cls]
                 # Draw rectangle
-                cv2.rectangle(frame, (x1,y1), (x2,y2), color, 2)
+                cv2.rectangle(frame, (x1,y1), (x2,y2), 2)
                 # Apply class name and confidence on image
-                cv2.putText(frame, f'{classes_names[int(box.cls[0])]}{box.conf[0]:.2f}', (x1,y1), cv2.FONT_HERSHEY_COMPLEX, 1, color, 2)
-                # Deer seen variable for sound triggering and video capture
-                deerSeen = True
-                out.write(frame)
-        
-        # Show the final image
-        cv2.imshow('frame',frame)
-        
-        # Break loop is 'q' is pressed
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+                cv2.putText(frame, f'{class_name[int(box.cls[0])]}{box.conf[0]:.2f}', (x1,y1), cv2.FONT_HERSHEY_COMPLEX, 1, 2)
 
-# Clean up
-out.release()
-videoCap.release()
+                if dclass==0.0:
+                    if pygame.mixer.music.get_busy():   # Keep busy while audio playing
+                        print("Audio is playing")
+                    else:
+                        # Load and play audio
+                        pygame.mixer.music.load("/path/to/audio.mp3")
+                        pygame.mixer.music.set_volume(1)
+                        pygame.mixer.music.play()
+                        
+cap.release()
 cv2.destroyAllWindows()
-        
